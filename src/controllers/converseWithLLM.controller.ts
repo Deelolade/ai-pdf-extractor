a310fe1c647ff7858d66c2fa2ba5f3eccce07c68
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-11-14 00:20:05 +0100

    gemini embeddings: configured the ai-embeddings but the embeddings are not stored in pincone yet

--- src/controllers/converseWithLLM.controller.ts
+++ src/controllers/converseWithLLM.controller.ts
@@ -5,6 +5,7 @@ import { apiKey, MAX_TRIALS, OPENAI_APIKEY, PINECONE_INDEX } from "../utils/env"
 import { Upload } from "../models/upload.model";
 import { pc } from "../utils/pinecone";
 import { User } from "../models/user.model";
+import { GoogleGenAI } from "@google/genai";
 
 const openai = new OpenAI({
     apiKey: apiKey,
@@ -14,6 +15,7 @@ const openai = new OpenAI({
 const openaiEmbed = new OpenAI({
     apiKey: OPENAI_APIKEY
 })
+const geminiEmbed = new GoogleGenAI({})
 export const converseWithLLM = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { uploadId } = req.params;
@@ -22,40 +24,60 @@ export const converseWithLLM = async (req: Request, res: Response, next: NextFun
             return next(errorHandler(400, "Message and Upload ID are required"))
         }
         const upload = await Upload.findOne({ _id: uploadId, userId: req.user?.id });
-        if(!upload){
+        if (!upload) {
             return next(errorHandler(404, "Upload not found !"))
         }
         const user = await User.findById(req.user?.id);
         if (!user) {
-          return next(errorHandler(404, "User not found"));
+            return next(errorHandler(404, "User not found"));
         }
-        // const queryEmbeddings = await openaiEmbed.embeddings.create({
-        //     model: "text-embedding-3-small",
-        //     input: message
-        // })
+        if (!user.isPaidUser) {
+            if (user.trialCount >= MAX_TRIALS) {
+                return next(errorHandler(403, "Trial limit reached. Please upgrade to a paid account."));
+            }
 
-        // const index = pc.index(PINECONE_INDEX);
+            user.trialCount += 1;
+            await user.save()
 
-        // if (!queryEmbeddings?.data?.[0]?.embedding) {
-        //     return next(errorHandler(500, "Failed to generate embeddings"))
-        // }
-        // console.log("Embedding length:", queryEmbeddings.data[0].embedding.length);
-        // const searchResults = await index.query({
-        //     topK: 5,
-        //     vector: queryEmbeddings.data[0].embedding,
-        //     includeMetadata: true,
-        //     filter: { fileId: uploadId }
-        // })
+            console.log("isPaidUser:", user.isPaidUser, "trialCount:", user.trialCount);
+        }
 
-        // const contextText = searchResults.matches
-        //     .map((m) => m.metadata?.text).join("\n\n")
+        const responseEmbeddings = await geminiEmbed.models.embedContent({
+            model: "gemini-embedding-001",
+            contents: message
+        })
+
+        const index = pc.index(PINECONE_INDEX);
+
+        if (!responseEmbeddings.embeddings) {
+            return next(errorHandler(500, "Failed to generate embeddings"))
+        }
+        if (!responseEmbeddings.embeddings?.length) {
+            return next(errorHandler(500, "Embedding array empty"));
+        }
+        const embeddings = responseEmbeddings.embeddings[0].values;
+        if (!embeddings) {
+            return next(errorHandler(500, "Failed to generate embeddings"));
+        }
+        const vector = embeddings
+
+
+        const searchResults = await index.query({
+            topK: 5,
+            vector,
+            includeMetadata: true,
+            filter: { fileId: uploadId.toString() }
+        })
+
+        const contextText = searchResults.matches
+            .map((m) => m.metadata?.text).join("\n\n")
         const systemPrompt = `
 You are an expert summarizer and document assistant.
 Answer based on this document content and previous summary.
 Document Title: ${upload?.fileName}
 Summary: ${upload?.summary}
+Relevant Chunks:\n${contextText}
 `;
-// Relevant Chunks:\n${contextText}
         const completion = await openai.chat.completions.create({
             model: "gpt-4o-mini",
             messages: [
@@ -64,21 +86,14 @@ Summary: ${upload?.summary}
             ]
         });
         const results = completion.choices[0].message?.content || "No response from LLM";
-        const updatedDocuments =  await Upload.findByIdAndUpdate(uploadId, {summary: results}, {new: true});
-        if(!user.isPaidUser){
-              if(user.trialCount >= MAX_TRIALS){
-                return next(errorHandler(403, "Trial limit reached. Please upgrade to a paid account."));
-              }
-        
-              user.trialCount += 1;
-              await user.save()
-              console.log("isPaidUser:", user.isPaidUser, "trialCount:", user.trialCount);
-            }
+        // const updatedDocuments = await Upload.findByIdAndUpdate(uploadId, { summary: results }, { new: true });
+        // Upload.wordCount = message.split(' ').length
+        // await
         res.status(200).json({
             success: true,
             message: "Response generated successfully",
-            // results,
-            updatedDocuments:updatedDocuments?.summary
+            results,
+            // updatedDocuments: updatedDocuments?.summary
         })
     } catch (error) {
         console.log(error)

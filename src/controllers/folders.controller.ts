7278abe1c99d4af10fb28f4133124f39c85f16f2
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-11-23 01:05:59 +0100

    folders: added the get all document folders and included pagination

--- src/controllers/folders.controller.ts
+++ src/controllers/folders.controller.ts
@@ -31,3 +31,23 @@ export const createFolder = async (req: Request, res: Response, next: NextFuncti
         next(errorHandler(500, "Failed to create folder"))
     }
 };
+export const getAllUserFolders = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const page = parseInt(req.query.page as string) || 1;
+        const limit = parseInt(req.query.limit as string) || 10;
+
+        const skip = (page - 1) * limit;
+        const userId = req.user?.id;
+        if(!userId){
+            return next(errorHandler(401, "Unauthorized"))
+        }
+        const folders = await Folder.find({userId}).limit(limit).skip(skip).sort({createdAt: -1});
+        res.status(200).json({
+            success: true,
+            folders
+        })
+    } catch (error) {
+        console.log(error)
+        next(errorHandler(500, "Failed to fetch folders"))
+    }
+}
--- src/routes/folders.route.ts
+++ src/routes/folders.route.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import { authenticateUser } from '../middleware/authMiddleware';
-import { createFolder } from '../controllers/folders.controller';
+import { createFolder, getAllUserFolders } from '../controllers/folders.controller';
 
 export const folderRouter = express.Router();
 
 folderRouter.post('/create', authenticateUser, createFolder)
+folderRouter.get('/', authenticateUser, getAllUserFolders)
